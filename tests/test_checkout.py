1fc3e117fdfdf31795bd49097af0b879e426ff1a
Breno <brenocs001@outlook.com>
2026-06-06 16:54:30 -0300

    Code Refactoring
    Código Refatorado (Funções "def" adcionadas)
    
    Code Refactoring (Function "def" added)

--- tests/test_checkout.py
+++ tests/test_checkout.py
@@ -1,5 +1,44 @@
 from playwright.sync_api import sync_playwright, expect
 
+def login(page, username, password):
+    page.locator("[data-test=\"username\"]").fill(username)
+    page.locator("[data-test=\"password\"]").fill(password)
+    page.locator("[data-test=\"login-button\"]").click()
+
+def add_to_cart(page):
+    page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+    page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+    page.locator(".shopping_cart_link").click()
+
+def checkout(page, first_name= "", last_name= "", code= ""):
+    page.locator('[data-test="checkout"]').click()
+
+    page.locator('[data-test="firstName"]').fill(first_name)
+    page.locator('[data-test="lastName"]').fill(last_name)
+    page.locator('[data-test="postalCode"]').fill(code)
+
+    page.locator('[data-test="continue"]').click()
+
+def verify_itens(page, final_products):
+    checkout = page.locator('[data-test="inventory-item"]')
+    assert checkout.count() == len(final_products)
+
+    for produto in final_products:
+        expect(page.locator("body")).to_contain_text(produto)
+
+def finish(page):
+    page.locator('[data-test="finish"]').click()
+
+    expect(page).to_have_url("https://www.saucedemo.com/checkout-complete.html")
+    expect(page.locator('[data-test="complete-header"]')).to_have_text("Thank you for your order!")
+
+    page.locator('[data-test="back-to-products"]').click()
+
+def verify_error(page, message):
+    error = page.locator(".error-message-container")
+    expect(error).to_have_text(message)
+
+# Checkout withou FirstName
 with sync_playwright() as p:
     browser = p.chromium.launch(headless= False)
     page = browser.new_page()
@@ -7,32 +46,70 @@ with sync_playwright() as p:
     page.goto("https://www.saucedemo.com/")
 
     # Login
-    page.locator("[data-test=\"username\"]").fill("standard_user")
-    page.locator("[data-test=\"password\"]").fill("secret_sauce")
-    page.locator("[data-test=\"login-button\"]").click()
-
+    login(page,"standard_user", "secret_sauce")
+    
     #Add item no carrinho
-    page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click()
-    page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
-    page.locator(".shopping_cart_link").click()
+    add_to_cart(page)
 
     #Checkout
-    page.locator('[data-test="checkout"]').click()
+    checkout(page, "", "de Tal", "123456") 
 
-    page.locator('[data-test="firstName"]').fill("Fulano")
-    page.locator('[data-test="lastName"]').fill("De Tal")
-    page.locator('[data-test="postalCode"]').fill("123456")
+    verify_error(page, "Error: First Name is required")
 
-    page.locator('[data-test="continue"]').click()
+# Checkout without LastName
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless= False)
+    page = browser.new_page()
+  
+    page.goto("https://www.saucedemo.com/")
+
+    # Login
+    login(page,"standard_user", "secret_sauce")
+    
+    #Add item no carrinho
+    add_to_cart(page)
+
+    #Checkout
+    checkout(page, "Fulano", "", "123456") 
+
+    verify_error(page, "Error: Last Name is required")
+
+# Checkout without Postal Code
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless= False)
+    page = browser.new_page()
+  
+    page.goto("https://www.saucedemo.com/")
+
+    # Login
+    login(page,"standard_user", "secret_sauce")
+    
+    #Add item no carrinho
+    add_to_cart(page)
+
+    #Checkout
+    checkout(page, "Fulano", "de Tal", "") 
+
+    verify_error(page, "Error: Postal Code is required")
+
+# Checkout Success
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless= False)
+    page = browser.new_page()
+  
+    page.goto("https://www.saucedemo.com/")
+
+    # Login
+    login(page,"standard_user", "secret_sauce")
+    
+    #Add item no carrinho
+    add_to_cart(page)
+
+    #Checkout
+    checkout(page, "Fulano", "de Tal", "123456")
 
     # Check Itens
-    expect(page.locator('[data-test="item-4-title-link"]')).to_contain_text("Sauce Labs Backpack")
-    expect(page.locator('[data-test="item-1-title-link"]')).to_contain_text("Sauce Labs Bolt T-Shirt")
+    verify_itens(page, ["Sauce Labs Backpack","Sauce Labs Bolt T-Shirt"])
 
     #Finish
-    page.locator('[data-test="finish"]').click()
-
-    expect(page).to_have_url("https://www.saucedemo.com/checkout-complete.html")
-    expect(page.locator('[data-test="complete-header"]')).to_have_text("Thank you for your order!")
-
-    page.locator('[data-test="back-to-products"]').click()
+    finish(page)
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,30 +1,30 @@
 from playwright.sync_api import sync_playwright, expect
 
+def login(page, username= "", password= ""):
+    page.locator("[data-test=\"username\"]").fill(username)
+    page.locator("[data-test=\"password\"]").fill(password)
+    page.locator("[data-test=\"login-button\"]").click()
+
 #Login with empty field
 with sync_playwright() as p:
     browser = p.chromium.launch(headless= False)
-
     page = browser.new_page()
 
     page.goto("https://www.saucedemo.com/")
-    
-    page.locator("[data-test=\"login-button\"]").click()
+
+    login(page)
 
     error = page.locator(".error-message-container")
     expect(error).to_have_text("Epic sadface: Username is required")
 
-    browser.close()
-
 #Login without password
 with sync_playwright() as p:
     browser = p.chromium.launch(headless= False)
-
     page = browser.new_page()
 
     page.goto("https://www.saucedemo.com/")
 
-    page.locator("[data-test=\"username\"]").fill("testeabc")
-    page.locator("[data-test=\"login-button\"]").click()
+    login(page, "teste123", "")
 
     error = page.locator(".error-message-container")
     expect(error).to_have_text("Epic sadface: Password is required")
@@ -35,13 +35,11 @@ with sync_playwright() as p:
 with sync_playwright() as p:
 
     browser = p.chromium.launch(headless= False)
-
     page = browser.new_page()
 
     page.goto("https://www.saucedemo.com/")
 
-    page.locator("[data-test=\"password\"]").fill("1234")
-    page.locator("[data-test=\"login-button\"]").click()
+    login(page, "", "12345")
 
     error = page.locator(".error-message-container")
     expect(error).to_have_text("Epic sadface: Username is required")
@@ -51,14 +49,11 @@ with sync_playwright() as p:
 #Invalid Login
 with sync_playwright() as p:
     browser = p.chromium.launch(headless= False)
-
     page = browser.new_page()
 
     page.goto("https://www.saucedemo.com/")
     
-    page.locator("[data-test=\"username\"]").fill("teste123")
-    page.locator("[data-test=\"password\"]").fill("12345")
-    page.locator("[data-test=\"login-button\"]").click()
+    login(page, "teste123", "12345")
 
     error = page.locator(".error-message-container")
     expect(error).to_have_text("Epic sadface: Username and password do not match any user in this service")
@@ -68,14 +63,11 @@ with sync_playwright() as p:
 #Sucess Login
 with sync_playwright() as p:
     browser = p.chromium.launch(headless= False)
-
     page = browser.new_page()
 
     page.goto("https://www.saucedemo.com/")
     
-    page.locator("[data-test=\"username\"]").fill("standard_user")
-    page.locator("[data-test=\"password\"]").fill("secret_sauce")
-    page.locator("[data-test=\"login-button\"]").click()
+    login(page, "standard_user", "secret_sauce")
 
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
 
